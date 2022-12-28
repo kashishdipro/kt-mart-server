5763414152528bb844cc7b2ea7013e044c8de0ec
kashishdipro <kamanashishdatta@gmail.com>
2022-12-28 19:20:41 +0600

    delete user from db

--- index.js
+++ index.js
@@ -213,6 +213,15 @@ async function run(){
             res.send(result);
         })
 
+        // Delete user Api for seller
+        app.delete('/users/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await userCollection.deleteOne(query);
+            res.send(result);
+        })
+
+
     }finally{
 
     }
